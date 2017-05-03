48e9d256e85564b6a28a930d01055136b90d4fbc
Dan Hoying <danhoying@gmail.com>
2017-05-03 12:20:42 -0700

    Initialize text field and operand variables

--- app/src/main/java/com/example/dan/calculator/MainActivity.java
+++ app/src/main/java/com/example/dan/calculator/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.dan.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+
+    // Variables to hold the operands and type of calculations
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
